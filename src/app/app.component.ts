c1ce4ee1401ab01b275252a3e01c97b268378960
Divyaraj Makwana <divyaraj.makwana9425@gmail.com>
2024-04-08 15:21:22 +0530

    2 way binding with form control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  answer='';
+  defaultQues = 'teacher';
   @ViewChild('f') formdata:NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
